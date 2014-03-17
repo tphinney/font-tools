64193d50c6d8be098601b683d8949ef6a1314e08
Thomas Phinney <tphinney@cal.berkeley.edu>
2014-03-16 21:46:34 -0700

    Create Generate-substitutions.py
    FontLab Studio 5 script to generate OpenType feature code for selected glyphs. Select the glyphs that are outputs of OpenType features, and it will generate code for you.

---
+++ Generate-substitutions.py
@@ -0,0 +1,23 @@
+# TWP: Generate OpenType feature substitution code for selected glyphs
+# works with simple ligatures and alternates.
+# Does not attempt to decode multiple-feature glyphs such as "f_f_i.alt"
+# as there is no single clear right input set for that
+
+f = fl.font
+if f == None:
+  exit
+
+def printCode(g, index):
+  if g == None:
+    return
+  dropDot = glyphs[index].name.split(".")[0]
+  inputNames = dropDot.replace("_"," ")
+  if inputNames = glyphs[index].name:
+  	return
+  print "  sub", inputNames, "by", glyphs[index].name , ";"
+
+font = fl.font
+glyphs = font.glyphs
+for index in range(len(glyphs)):
+  if fl.Selected(index):
+    printCode(glyphs[index], index)
